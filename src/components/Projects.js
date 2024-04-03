d5b108cf4764e427e4fe5ce3d77254a90e547f03
wjal <wjalittle@myseneca.ca>
2024-04-02 20:27:13 -0400

    cotask project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,5 +19,5 @@ const Projects = ({onChangeContent}) => {
 const portfolioProjects = [
 {type: 'Project', name: 'Electric Savings', image: 'electricScreenShot.png', description: `Single page mobile app for calculating annual savings by using an electric vehicle vs the cost of gas for an ICE vehicle.`, movie: `https://player.vimeo.com/video/924532972?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`, comingSoon: 'COMING SOON'},
 {type: 'Project', name: 'Get Weather', image: 'getWeatherScreenshot.png', description: `A react web application for searching for weather around the world.  Users can enable geolocation to automatically receive weather conditions for their current location`, movie: 'https://player.vimeo.com/video/924540918?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479', comingSoon: 'COMING SOON'},
-{type: 'Project', name: 'Bill Splitter', image: 'proj3.png', description: 'Single page react application, which enables users to add friends, and split payments.', movie: null, comingSoon: 'COMING SOON'}
+{type: 'Project', name: 'CoTask', image: 'proj3.png', description: 'Task and Goal manager, that allows users to keep schedule their day, and add tasks they are attempting to accomplish.  Users can create shared tasks and schedules with friends', movie: null, comingSoon: 'COMING SOON'}
 ]
